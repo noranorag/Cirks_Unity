8c6fec480d11f5703ea53e4225b453dfb285c21d
Nora <noragalina167@gmail.com>
2025-02-06 12:15:14 +0200

    Izveidots sakumekrans tads biski smukaks, un characters

--- Assets/Scripts/PlayersScript.cs
+++ Assets/Scripts/PlayersScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.IO;
 
 public class PlayersScript : MonoBehaviour
 {
@@ -16,12 +17,32 @@ public class PlayersScript : MonoBehaviour
         characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
         GameObject mainCharacter = Instantiate(playerPrefabs[characterIndex], spawnPoint.transform.position, Quaternion.identity);
         mainCharacter.GetComponent<NameScript>().SetPlayerName(PlayerPrefs.GetString("PlayerName"));
+
+        otherPlayers = new int[PlayerPrefs.GetInt("PlayerCount")];
+        string[] nameArray = ReadLinesFromFile(textFileName);
+        for (int i = 0; i < otherPlayers.Length-1; i++)
+        {
+            spawnPoint.transform.position += new Vector3(0.2f, 0, 0.08f);
+            index = Random.Range(0, playerPrefabs.Length-1);
+            GameObject character = Instantiate(playerPrefabs[index], spawnPoint.transform.position, Quaternion.identity);
+            character.GetComponent<NameScript>().SetPlayerName(nameArray[Random.Range(0, nameArray.Length-1)]);
+        }
+
   
     }
 
-    // Update is called once per frame
-    void Update()
+    string[] ReadLinesFromFile(string fName)
     {
-        
+        TextAsset textAsset = Resources.Load<TextAsset>(fName);
+        if (textAsset != null)
+        {
+            return textAsset.text.Split(new[] {'\r', '\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+        }
+        else
+        {
+            Debug.LogError("File not found: " + fName);
+            return new string[0];
+        }
     }
+    
 }
