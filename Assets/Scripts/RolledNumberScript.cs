be62268d5b6eaaf208e63cfbfcdd83b2fa440c11
Nora <noragalina167@gmail.com>
2025-02-11 09:19:43 +0200

    Izveidots metamais kaulins ka ari izveidots metama kaulina cipara thing

---
+++ Assets/Scripts/RolledNumberScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RolledNumberScript : MonoBehaviour
+{
+    DiceRollScript diceRollScript;
+    [SerializeField] TMP_Text rolledNumberText;
+
+    void Awake()
+    {
+        diceRollScript = FindObjectOfType<DiceRollScript>();
+    }
+
+    
+    void Update()
+    {
+        if(diceRollScript != null)
+            if (diceRollScript.isLanded)
+                rolledNumberText.text = diceRollScript.diceFaceNum;
+            else
+                rolledNumberText.text = "?";
+
+            else
+                Debug.LogError("DiceRollScript not found in a scene!");
+    }
+}
